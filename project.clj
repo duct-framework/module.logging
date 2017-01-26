eafa9a3be27282656deff4bc9b50848f3b095204
James Reeves <jreeves@weavejester.com>
2017-01-26 02:44:12 +0000

    Add Timbre and Integrant dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [integrant "0.2.0"]])
