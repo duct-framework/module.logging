abba36d086b8e716a864ab242a2fb0b177ba1fd2
James Reeves <jreeves@weavejester.com>
2017-01-26 02:13:53 +0000

    Initial commit

---
+++ test/duct/logger/timbre_test.clj
@@ -0,0 +1,7 @@
+(ns duct.logger.timbre-test
+  (:require [clojure.test :refer :all]
+            [duct.logger.timbre :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
