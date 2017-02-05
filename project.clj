ec206c535e41ed2c3df517a9943475c7c23edc34
James Reeves <jreeves@weavejester.com>
2017-02-05 17:22:34 +0000

    Rename project to duct/module.logging

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/logger.timbre "0.1.0-SNAPSHOT"
+(defproject duct/module.logging "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
