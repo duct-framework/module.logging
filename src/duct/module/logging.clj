3c19fde40291b8e59c26ed4479769e018be5dec5
James Reeves <jreeves@weavejester.com>
2017-02-07 22:37:45 +0000

    Add support for environments
    In production, we default to logging to STDOUT, while in development we
    log to a dev.log file.

--- src/duct/module/logging.clj
+++ src/duct/module/logging.clj
@@ -11,10 +11,13 @@
       (assoc-in-default [:duct.logger/timbre :appenders key] (ig/ref key))
       (update key (partial meta-merge options))))
 
-(defmethod ig/init-key :duct.module/logging [_ _]
+(defmethod ig/init-key :duct.module/logging [_ options]
   (fn [config]
-    (-> config
-        (assoc-in-default [:duct.logger/timbre :level] :info)
-        (add-appender ::timbre/println {:stream :auto})
-        (web/add-middleware ::mw/binding         (ig/ref :duct.logger/timbre))
-        (web/add-middleware ::mw/request-logging (ig/ref :duct.logger/timbre)))))
+    (let [env (:enviroment options (:duct.core/environment config :production))]
+      (-> config
+          (assoc-in-default [:duct.logger/timbre :level] :info)
+          (cond->
+            (= env :production)  (add-appender ::timbre/println {:stream :auto})
+            (= env :development) (add-appender ::timbre/spit    {:fname "logs/dev.log"}))
+          (web/add-middleware ::mw/binding         (ig/ref :duct.logger/timbre))
+          (web/add-middleware ::mw/request-logging (ig/ref :duct.logger/timbre))))))
