537b82c0f2561f0144b5f42d907c47c2eaef234a
James Reeves <jreeves@weavejester.com>
2017-02-10 17:45:56 +0000

    Move duct.timbre.logger into separate project

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.8.0"]
                  [duct/core "0.9.0-SNAPSHOT"]
                  [duct/module.web "0.1.0-SNAPSHOT"]
+                 [duct/logger.timbre "0.1.0-SNAPSHOT"]
                  [integrant "0.2.0"]
                  [meta-merge "1.0.0"]])
