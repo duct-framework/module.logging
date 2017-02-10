484c5249680dee2a3988850c81ae0caec30f947a
James Reeves <jreeves@weavejester.com>
2017-02-10 17:46:18 +0000

    Update Clojure dependency to 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/core "0.9.0-SNAPSHOT"]
                  [duct/module.web "0.1.0-SNAPSHOT"]
                  [duct/logger.timbre "0.1.0-SNAPSHOT"]
