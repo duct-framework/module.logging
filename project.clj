9a701fcc1a86756053e45c3e9a0b3933fa0ca6f0
James Reeves <jreeves@weavejester.com>
2017-04-13 22:04:35 +0100

    Update logger.timbre to 0.1.0

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/core "0.1.0-SNAPSHOT"]
-                 [duct/logger.timbre "0.1.0-SNAPSHOT"]
+                 [duct/logger.timbre "0.1.0"]
                  [integrant "0.3.3"]
                  [meta-merge "1.0.0"]])
