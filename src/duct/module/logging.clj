d1491a9956cf9b8bc251428d90f0d311d648a435
James Reeves <jreeves@weavejester.com>
2017-04-14 16:26:51 +0100

    Refactor configuration merging

--- src/duct/module/logging.clj
+++ src/duct/module/logging.clj
@@ -1,5 +1,6 @@
 (ns duct.module.logging
   (:require [duct.core :as core]
+            [duct.core.merge :as merge]
             [duct.logger :as logger]
             [duct.logger.timbre :as timbre]
             [integrant.core :as ig]))
@@ -7,22 +8,25 @@
 (defn- get-environment [config options]
   (:enviroment options (::core/environment config :production)))
 
-(defn- set-default-log-level [config level]
-  (core/merge-configs config {::logger/timbre ^:displace {:level level}}))
+(def ^:private prod-config
+  {::logger/timbre
+   {:level     (merge/displace :info)
+    :appenders ^:displace {::timbre/println (ig/ref ::timbre/println)}}
+   ::timbre/println {}})
 
-(defn- add-appender [config key options]
-  (core/merge-configs config {::logger/timbre {:appenders {key (ig/ref key)}}
-                              key options}))
+(def ^:private dev-config
+  {::logger/timbre
+   {:level     (merge/displace :debug)
+    :appenders ^:displace {::timbre/spit  (ig/ref ::timbre/spit)
+                           ::timbre/brief (ig/ref ::timbre/brief)}}
+   ::timbre/spit
+   {:fname (merge/displace "logs/dev.log")}
+   ::timbre/brief {}})
+
+(def ^:private env-configs
+  {:production prod-config
+   :development dev-config})
 
 (defmethod ig/init-key :duct.module/logging [_ options]
   (fn [config]
-    (case (get-environment config options)
-      :production
-      (-> config
-          (set-default-log-level :info)
-          (add-appender ::timbre/println {}))
-      :development
-      (-> config
-          (set-default-log-level :debug)
-          (add-appender ::timbre/spit  {:fname "logs/dev.log"})
-          (add-appender ::timbre/brief {})))))
+    (core/merge-configs config (env-configs (get-environment config options)))))
