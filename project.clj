60b3106da8ed5891ecadb143ec731125eb19d23c
James Reeves <jreeves@weavejester.com>
2018-02-19 15:41:04 +0000

    Update Clojure version to 1.9.0 stable

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/module.logging"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [duct/core "0.6.1"]
                  [duct/logger.timbre "0.4.1"]
                  [integrant "0.6.1"]])
