8dccb35af8c1dd8e47a0f1ef8be75834462dbace
James Reeves <jreeves@weavejester.com>
2024-10-24 23:52:20 +0100

    Update module to use Integrant's expand function
    This breaks compatibility with Duct Core's old module system.

--- src/duct/module/logging.clj
+++ src/duct/module/logging.clj
@@ -1,38 +1,13 @@
 (ns duct.module.logging
-  (:require [duct.core :as core]
-            [duct.core.merge :as merge]
-            [integrant.core :as ig]))
+  (:require [integrant.core :as ig]))
 
-(defn- get-environment [config options]
-  (:environment options (::core/environment config :production)))
-
-(def ^:private prod-config
-  {:duct.logger/timbre
-   {:level     (merge/displace :info)
-    :appenders ^:displace {:duct.logger.timbre/println (ig/ref :duct.logger.timbre/println)}}
-   :duct.logger.timbre/println {}})
-
-(def ^:private test-config
-  {:duct.logger/timbre
-   {:level     (merge/displace :debug)
-    :appenders ^:displace {:duct.logger.timbre/spit  (ig/ref :duct.logger.timbre/spit)}}
-   :duct.logger.timbre/spit
-   {:fname (merge/displace "logs/test.log")}})
-
-(def ^:private dev-config
-  {:duct.logger/timbre
-   {:level     (merge/displace :debug)
-    :appenders ^:displace {:duct.logger.timbre/spit  (ig/ref :duct.logger.timbre/spit)
-                           :duct.logger.timbre/brief (ig/ref :duct.logger.timbre/brief)}}
-   :duct.logger.timbre/spit
-   {:fname (merge/displace "logs/dev.log")}
-   :duct.logger.timbre/brief
-   {:min-level (merge/displace :report)}})
-
-(def ^:private env-configs
-  {:production prod-config
-   :development dev-config
-   :test       test-config})
-
-(defmethod ig/init-key :duct.module/logging [_ options]
-  #(core/merge-configs % (env-configs (get-environment % options))))
+(defmethod ig/expand-key :duct.module/logging [_ _]
+  {:duct.logger/simple
+   (ig/profile
+    :dev  {:appenders
+           [{:type :stdout, :timestamps? false, :levels #{:report}}
+            {:type :file, :path "logs/dev.log"}]}
+    :test {:appenders
+           [{:type :file, :path "logs/test.log"}]}
+    :prod {:appenders
+           [{:type :stdout}]})})
--- test/duct/module/logging_test.clj
+++ test/duct/module/logging_test.clj
@@ -1,81 +1,18 @@
 (ns duct.module.logging-test
-  (:require [clojure.test :refer :all]
-            [duct.core :as core]
+  (:require [clojure.test :refer [deftest is]]
             [duct.module.logging :as logging]
             [integrant.core :as ig]))
 
-(core/load-hierarchy)
-
-(def base-config
-  {:duct.module/logging {}})
-
 (deftest module-test
-  (testing "blank config"
-    (is (= {:duct.logger/timbre
-            {:level :info
-             :appenders
-             {:duct.logger.timbre/println (ig/ref :duct.logger.timbre/println)}}
-            :duct.logger.timbre/println {}}
-           (core/build-config base-config))))
-
-  (testing "config with log level"
-    (let [config (assoc base-config :duct.profile/base {:duct/logger {:level :warn}})]
-      (is (= {:duct.logger/timbre
-              {:level :warn
-               :appenders
-               {:duct.logger.timbre/println (ig/ref :duct.logger.timbre/println)}}
-              :duct.logger.timbre/println {}}
-             (core/build-config config)))))
-
-  (testing "development environment"
-    (let [config (assoc base-config
-                        :duct.profile/base
-                        {:duct.core/environment :development})]
-      (is (= {:duct.logger/timbre
-              {:level :debug
-               :appenders
-               {:duct.logger.timbre/spit  (ig/ref :duct.logger.timbre/spit)
-                :duct.logger.timbre/brief (ig/ref :duct.logger.timbre/brief)}}
-              :duct.logger.timbre/spit  {:fname "logs/dev.log"}
-              :duct.logger.timbre/brief {:min-level :report}
-              :duct.core/environment :development}
-             (core/build-config config)))))
-
-  (testing "test environment"
-    (let [config (assoc base-config
-                        :duct.profile/base
-                        {:duct.core/environment :test})]
-      (is (= {:duct.logger/timbre
-              {:level :debug
-               :appenders
-               {:duct.logger.timbre/spit (ig/ref :duct.logger.timbre/spit)}}
-              :duct.logger.timbre/spit {:fname "logs/test.log"}
-              :duct.core/environment :test}
-             (core/build-config config)))))
-
-  (testing "environment override"
-    (let [config {:duct.module/logging {:environment :development}}]
-      (is (= {:duct.logger/timbre
-              {:level :debug
-               :appenders
-               {:duct.logger.timbre/spit  (ig/ref :duct.logger.timbre/spit)
-                :duct.logger.timbre/brief (ig/ref :duct.logger.timbre/brief)}}
-              :duct.logger.timbre/spit  {:fname "logs/dev.log"}
-              :duct.logger.timbre/brief {:min-level :report}}
-             (core/build-config config)))))
-
-  (testing "config with min log level and file name"
-    (let [config (assoc base-config
-                        :duct.profile/base
-                        {:duct.core/environment :development
-                         :duct.logger.timbre/spit  {:fname "custom.log"}
-                         :duct.logger.timbre/brief {:min-level :info}})]
-      (is (= {:duct.logger/timbre
-              {:level :debug
-               :appenders
-               {:duct.logger.timbre/spit  (ig/ref :duct.logger.timbre/spit)
-                :duct.logger.timbre/brief (ig/ref :duct.logger.timbre/brief)}}
-              :duct.logger.timbre/spit  {:fname "custom.log"}
-              :duct.logger.timbre/brief {:min-level :info}
-              :duct.core/environment :development}
-             (core/build-config config))))))
+  (let [config {:duct.module/logging {}}]
+    (is (= {:duct.logger/simple
+            {:appenders [{:type :stdout}]}}
+           (ig/expand config (ig/deprofile [:prod]))))
+    (is (= {:duct.logger/simple
+            {:appenders [{:type :file, :path "logs/test.log"}]}}
+           (ig/expand config (ig/deprofile [:test]))))
+    (is (= {:duct.logger/simple
+            {:appenders
+             [{:type :stdout, :timestamps? false, :levels #{:report}}
+              {:type :file, :path "logs/dev.log"}]}}
+           (ig/expand config (ig/deprofile [:dev]))))))
