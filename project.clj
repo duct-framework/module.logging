78413e3016e545811c62a20ba2a0b8924d1fb6b9
James Reeves <jreeves@weavejester.com>
2024-11-04 01:42:00 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
-                 [org.duct-framework/logger "0.3.0"]
-                 [org.duct-framework/logger.simple "0.4.2"]
-                 [integrant "0.13.0"]])
+                 [org.duct-framework/logger "0.4.0"]
+                 [org.duct-framework/logger.simple "0.4.3"]
+                 [integrant "0.13.1"]])
