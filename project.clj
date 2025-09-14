ef8c31e099ed338a05c76ef4e0fa5c51f0db8c5b
James Reeves <jreeves@weavejester.com>
2025-09-14 15:18:42 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/module.logging"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [org.duct-framework/logger "0.4.0"]
-                 [org.duct-framework/logger.simple "0.4.6"]
-                 [integrant "0.13.1"]])
+                 [org.duct-framework/logger.simple "0.4.7"]
+                 [integrant "1.0.0"]])
